59e1d6254f4c9433b30ae797ea41e23e969419cd
kurisu <callmekurisu@gmail.com>
2018-11-16 19:04:01 -0600

    First successful build with artifacts!!!

--- client/src/Components/SignIn/UserSignIn.component.js
+++ client/src/Components/SignIn/UserSignIn.component.js
@@ -38,6 +38,7 @@ export class UserSignInComponent extends React.Component {
        //window.location.assign('127.0.0.1/404')
         console.log(err);
       })
+      console.log("We are trying to hit the server")
   }
 
   render() {
