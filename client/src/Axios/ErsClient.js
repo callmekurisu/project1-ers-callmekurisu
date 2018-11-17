42697459c6f4e4f65493e1d65bc637f11dac3ca0
kurisu <callmekurisu@gmail.com>
2018-11-16 20:21:56 -0600

    env testing still

--- client/src/Axios/ErsClient.js
+++ client/src/Axios/ErsClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { environment } from './Environment';
 
 const ErsClient = axios.create({ 
-baseURL:`${environment}`,
+baseURL:`${environment.context}`,
 headers: {
     'Content-Type': 'application/json'
   },
